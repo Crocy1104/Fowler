155bc7947b4264da9bcba7286dd38669d58e8ecb
Crocy1104 <marcocrocoll@gmx.de>
2017-05-10 15:45:04 +0200

    changed get/set

--- src/Customer.java
+++ src/Customer.java
@@ -5,11 +5,11 @@ import java.util.*;
 class Customer {
     private String name;
     private Vector rentals = new Vector();
-    public Customer (String newname){
-        name = newname;
+    public Customer (String name){
+        this.name = name;
     };
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
+        this.rentals.addElement(arg);
         
     };
     public String getName (){
